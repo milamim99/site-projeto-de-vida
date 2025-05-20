bf1bb2ad390e149ff12beabdf4de5f826988bfdf
milamim99 <camila.muniz06@escola.pr.gov.br>
2025-05-20 11:40:13 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const botoes = document.querySelectorAll("botao");
-
+  const texto = document.querySelectorAll  (".aba-conteudo"); 
 for (let i = 0; i < botoes.length; i++)  {
     botoes[i].onclick = function () {
         for (let  j = 0; j < botoes.length;  j++) {
+
+     for (let j = 0; j <botoes.length; j++){
+         botoes[j].classList.remove("ativo"){
+         textos[j].classList.remove("ativo");
+         }
+
+         botoes[i].classList.add("ativo");
+         textos[i].classList.add("ativo");
+        };
+}
+  
+const contadores = document.querySelectorAll  ("contador");
+  const tempo()objetivo1 = new Date ("2025-12-03t00.00.00");
+  
+
